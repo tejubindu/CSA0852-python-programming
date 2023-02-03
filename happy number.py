dc604297e17d997e89ce0dd9c9deabb9ec591744
tejubindu <113327752+tejubindu@users.noreply.github.com>
2023-02-03 14:18:29 +0530

    Add files via upload

---
+++ happy number.py
@@ -0,0 +1,10 @@
+num=int(input("num:"))
+def happy_num(n):
+    past=set()
+    while n!=1:
+        n=sum(int(i)**2 for i in str(n))
+        if n in past:
+            return False
+        past.add(n)
+    return True
+print(happy_num(num))
